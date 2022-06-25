ba6110bff557ce755cde6f3a77673ed17743e3b5
Fauconfan <joseph0priou@gmail.com>
2022-06-25 22:01:23 +0200

    putting all in a macro, to implement f32

--- src/lib.rs
+++ src/lib.rs
@@ -3,71 +3,80 @@ use std::fmt::{Debug, Display, Formatter};
 use std::hash::{Hash, Hasher};
 use std::num::FpCategory::{Infinite, Nan};
 
-#[allow(non_camel_case_types)]
-#[derive(PartialEq, PartialOrd, Clone, Copy)]
-pub struct ff64 {
-    x: f64,
-}
-
-impl ff64 {
-    pub fn from(x: f64) -> ff64 {
-        match x.classify() {
-            Nan | Infinite => panic!("Cannot create `fix f64` from {}", x),
-            _ => {}
+macro_rules! _impl_ty {
+    ($base:ty, $new:ident) => {
+        #[allow(non_camel_case_types)]
+        #[derive(PartialEq, PartialOrd, Clone, Copy)]
+        pub struct $new {
+            x: $base,
         }
 
-        ff64 { x }
-    }
-}
+        impl $new {
+            pub fn from(x: $base) -> $new {
+                match x.classify() {
+                    Nan | Infinite => {
+                        panic!("Cannot create `fix {}` from {}", stringify!($base), x)
+                    }
+                    _ => {}
+                }
 
-impl Eq for ff64 {}
-
-impl Ord for ff64 {
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.partial_cmp(other).unwrap()
-    }
-}
-
-impl Debug for ff64 {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "fix {}", self.x)
-    }
-}
-
-impl Display for ff64 {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "fix {}", self.x)
-    }
-}
-
-impl Hash for ff64 {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        if self.x == 0.0 {
-            (0.0 as f64).to_bits().hash(state);
-        } else {
-            self.x.to_bits().hash(state)
+                $new { x }
+            }
         }
-    }
-}
 
-impl From<f64> for ff64 {
-    fn from(x: f64) -> Self {
-        ff64::from(x)
-    }
-}
+        impl Eq for $new {}
 
-impl From<ff64> for f64 {
-    fn from(x: ff64) -> Self {
-        x.x
-    }
-}
+        impl Ord for $new {
+            fn cmp(&self, other: &Self) -> Ordering {
+                self.partial_cmp(other).unwrap()
+            }
+        }
 
-#[macro_export]
-macro_rules! ff64 {
-    ($x:literal) => {
-        $crate::ff64::from($x)
+        impl Debug for $new {
+            fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+                write!(f, "fix {}", self.x)
+            }
+        }
+
+        impl Display for $new {
+            fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+                write!(f, "fix {}", self.x)
+            }
+        }
+
+        impl Hash for $new {
+            fn hash<H: Hasher>(&self, state: &mut H) {
+                if self.x == 0.0 {
+                    (0.0 as $base).to_bits().hash(state);
+                } else {
+                    self.x.to_bits().hash(state)
+                }
+            }
+        }
+
+        impl From<$base> for $new {
+            fn from(x: $base) -> Self {
+                $new::from(x)
+            }
+        }
+
+        impl From<$new> for $base {
+            fn from(x: $new) -> Self {
+                x.x
+            }
+        }
+
+        #[macro_export]
+        macro_rules! $new {
+            ($x:literal) => {
+                $crate::$new::from($x)
+            };
+            ($x:expr) => {
+                $crate::$new::from($x)
+            };
+        }
     };
-    ($x:expr) => {
-        $crate::ff64::from($x)
-    };
 }
+
+_impl_ty!(f64, ff64);
+_impl_ty!(f32, ff32);
--- tests/init.rs
+++ tests/init.rs
@@ -1,6 +1,6 @@
 use fix_float::*;
 
-const COMMON_FLOATS: &'static [f64] = &[
+const F64_COMMON_FLOATS: &'static [f64] = &[
     0.0,
     1.0,
     -1.0,
@@ -29,27 +29,81 @@ const COMMON_FLOATS: &'static [f64] = &[
     std::f64::consts::TAU,
 ];
 
+const F32_COMMON_FLOATS: &'static [f32] = &[
+    0.0,
+    1.0,
+    -1.0,
+    42.42,
+    f32::EPSILON,
+    f32::MIN,
+    f32::MAX,
+    std::f32::consts::E,
+    std::f32::consts::FRAC_1_PI,
+    std::f32::consts::FRAC_1_SQRT_2,
+    std::f32::consts::FRAC_2_PI,
+    std::f32::consts::FRAC_2_SQRT_PI,
+    std::f32::consts::FRAC_PI_2,
+    std::f32::consts::FRAC_PI_3,
+    std::f32::consts::FRAC_PI_4,
+    std::f32::consts::FRAC_PI_6,
+    std::f32::consts::FRAC_PI_8,
+    std::f32::consts::LN_2,
+    std::f32::consts::LN_10,
+    std::f32::consts::LOG2_10,
+    std::f32::consts::LOG2_E,
+    std::f32::consts::LOG10_2,
+    std::f32::consts::LOG10_E,
+    std::f32::consts::PI,
+    std::f32::consts::SQRT_2,
+    std::f32::consts::TAU,
+];
+
 #[test]
-fn common_floats() {
-    for &x in COMMON_FLOATS {
-        let _ = ff64::from(x);
+fn f64_common_floats() {
+    for &x in F64_COMMON_FLOATS {
+        let _ = ff64!(x);
     }
 }
 
 #[test]
 #[should_panic]
-fn nan() {
-    let _ = ff64::from(f64::NAN);
+fn f64_nan() {
+    let _ = ff64!(f64::NAN);
 }
 
 #[test]
 #[should_panic]
-fn inf() {
-    let _ = ff64::from(f64::INFINITY);
+fn f64_inf() {
+    let _ = ff64!(f64::INFINITY);
 }
 
 #[test]
 #[should_panic]
-fn neg_inf() {
-    let _ = ff64::from(f64::NEG_INFINITY);
+fn f64_neg_inf() {
+    let _ = ff64!(f64::NEG_INFINITY);
+}
+
+#[test]
+fn f32_common_floats() {
+    for &x in F32_COMMON_FLOATS {
+        let _ = ff32!(x);
+    }
+}
+
+#[test]
+#[should_panic]
+fn f32_nan() {
+    let _ = ff32!(f32::NAN);
+}
+
+#[test]
+#[should_panic]
+fn f32_inf() {
+    let _ = ff32!(f32::INFINITY);
+}
+
+#[test]
+#[should_panic]
+fn f32_neg_inf() {
+    let _ = ff32!(f32::NEG_INFINITY);
 }
