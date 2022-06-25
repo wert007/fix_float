ef0b697499b06196b06d54ebb3db62df5a977154
Fauconfan <joseph0priou@gmail.com>
2022-06-25 13:02:31 +0200

    poc

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,46 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+use std::cmp::Ordering;
+use std::fmt::Display;
+use std::num::FpCategory::*;
+
+#[allow(non_camel_case_types)]
+#[derive(Debug, PartialEq, PartialOrd)]
+pub struct ff64 {
+    x: f64,
+}
+
+impl ff64 {
+    pub fn from(x: f64) -> ff64 {
+        match x.classify() {
+            Nan | Infinite => panic!("Cannot create `fix f64` from {}", x),
+            _ => {}
+        }
+
+        ff64 { x }
+    }
+}
+
+impl Eq for ff64 {}
+
+impl Ord for ff64 {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.partial_cmp(other).unwrap()
+    }
+}
+
+impl Display for ff64 {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "fix {}", self.x)
+    }
+}
+
+impl From<f64> for ff64 {
+    fn from(x: f64) -> Self {
+        ff64::from(x)
+    }
+}
+
+impl From<ff64> for f64 {
+    fn from(x: ff64) -> Self {
+        x.x
     }
 }
