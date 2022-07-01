128b34bb470d6db97cc42b25cd7f60aebd1e9d15
Fauconfan <joseph0priou@gmail.com>
2022-07-01 16:22:16 +0200

    refacto tests

--- src/lib.rs
+++ src/lib.rs
@@ -107,3 +107,213 @@ macro_rules! _impl_ty {
 
 _impl_ty!(f64, ff64);
 _impl_ty!(f32, ff32);
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rand;
+
+    const F64_COMMON_FLOATS: &'static [f64] = &[
+        0.0,
+        1.0,
+        -1.0,
+        42.42,
+        f64::EPSILON,
+        f64::MIN,
+        f64::MAX,
+        std::f64::consts::E,
+        std::f64::consts::FRAC_1_PI,
+        std::f64::consts::FRAC_1_SQRT_2,
+        std::f64::consts::FRAC_2_PI,
+        std::f64::consts::FRAC_2_SQRT_PI,
+        std::f64::consts::FRAC_PI_2,
+        std::f64::consts::FRAC_PI_3,
+        std::f64::consts::FRAC_PI_4,
+        std::f64::consts::FRAC_PI_6,
+        std::f64::consts::FRAC_PI_8,
+        std::f64::consts::LN_2,
+        std::f64::consts::LN_10,
+        std::f64::consts::LOG2_10,
+        std::f64::consts::LOG2_E,
+        std::f64::consts::LOG10_2,
+        std::f64::consts::LOG10_E,
+        std::f64::consts::PI,
+        std::f64::consts::SQRT_2,
+        std::f64::consts::TAU,
+    ];
+
+    const F32_COMMON_FLOATS: &'static [f32] = &[
+        0.0,
+        1.0,
+        -1.0,
+        42.42,
+        f32::EPSILON,
+        f32::MIN,
+        f32::MAX,
+        std::f32::consts::E,
+        std::f32::consts::FRAC_1_PI,
+        std::f32::consts::FRAC_1_SQRT_2,
+        std::f32::consts::FRAC_2_PI,
+        std::f32::consts::FRAC_2_SQRT_PI,
+        std::f32::consts::FRAC_PI_2,
+        std::f32::consts::FRAC_PI_3,
+        std::f32::consts::FRAC_PI_4,
+        std::f32::consts::FRAC_PI_6,
+        std::f32::consts::FRAC_PI_8,
+        std::f32::consts::LN_2,
+        std::f32::consts::LN_10,
+        std::f32::consts::LOG2_10,
+        std::f32::consts::LOG2_E,
+        std::f32::consts::LOG10_2,
+        std::f32::consts::LOG10_E,
+        std::f32::consts::PI,
+        std::f32::consts::SQRT_2,
+        std::f32::consts::TAU,
+    ];
+
+    #[test]
+    fn ff64_common_floats() {
+        for &x in F64_COMMON_FLOATS {
+            let fx = ff64!(x);
+            assert_eq!(x, *fx)
+        }
+    }
+
+    #[test]
+    #[should_panic]
+    fn ff64_nan() {
+        let _ = ff64!(f64::NAN);
+    }
+
+    #[test]
+    #[should_panic]
+    fn ff64_inf() {
+        let _ = ff64!(f64::INFINITY);
+    }
+
+    #[test]
+    #[should_panic]
+    fn ff64_neg_inf() {
+        let _ = ff64!(f64::NEG_INFINITY);
+    }
+
+    #[test]
+    fn ff32_common_floats() {
+        for &x in F32_COMMON_FLOATS {
+            let fx = ff32!(x);
+            assert_eq!(x, *fx)
+        }
+    }
+
+    #[test]
+    #[should_panic]
+    fn ff32_nan() {
+        let _ = ff32!(f32::NAN);
+    }
+
+    #[test]
+    #[should_panic]
+    fn ff32_inf() {
+        let _ = ff32!(f32::INFINITY);
+    }
+
+    #[test]
+    #[should_panic]
+    fn ff32_neg_inf() {
+        let _ = ff32!(f32::NEG_INFINITY);
+    }
+
+    #[test]
+    fn default_ff64() {
+        let a = ff64::default();
+
+        assert_eq!(f64::from(a), f64::default());
+    }
+
+    #[test]
+    fn default_ff32() {
+        let a = ff32::default();
+
+        assert_eq!(f32::from(a), f32::default());
+    }
+
+    #[test]
+    fn eq_zero() {
+        let a = ff64!(0.0);
+        let b = ff64!(0.0);
+        let c = ff64!(-0.0);
+
+        assert_eq!(a, b);
+        assert_eq!(a, c);
+    }
+
+    #[test]
+    fn eq_fuzzy() {
+        for _ in 0..1000 {
+            let random_float = rand::random();
+            let a = ff64!(random_float);
+            let b = ff64!(random_float);
+
+            assert_eq!(a, b);
+        }
+    }
+
+    #[test]
+    fn fmt() {
+        assert_eq!(format!("{}", ff64!(2.42)), "fix 2.42");
+        assert_eq!(format!("{:?}", ff64!(2.42)), "fix 2.42");
+        assert_eq!(format!("{:}", ff64!(2.42)), "fix 2.42");
+        assert_eq!(format!("{:10}", ff64!(2.42)), "fix 2.42  ");
+        assert_eq!(format!("{:15}", ff64!(2.42)), "fix 2.42       ");
+        assert_eq!(format!("{:0<15}", ff64!(2.42)), "fix 2.420000000");
+        assert_eq!(format!("{:>15}", ff64!(2.42)), "       fix 2.42");
+        assert_eq!(format!("{:0>15}", ff64!(2.42)), "0000000fix 2.42");
+    }
+
+    #[test]
+    fn is_send() {
+        fn assert_send<T: Send>() {}
+
+        assert_send::<ff64>();
+        assert_send::<ff32>();
+    }
+
+    #[test]
+    fn is_sync() {
+        fn assert_sync<T: Sync>() {}
+
+        assert_sync::<ff64>();
+        assert_sync::<ff32>();
+    }
+
+    #[test]
+    fn try_from() {
+        let a: ff64 = ff64::try_from(42.42).unwrap();
+        let b: ff64 = (42.42).try_into().unwrap();
+        let c: ff64 = ff64!(42.42);
+
+        assert_eq!(a, b);
+        assert_eq!(a, c);
+
+        assert_eq!(*a, *b);
+        assert_eq!(*a, *c);
+    }
+
+    #[test]
+    fn try_from_nan() {
+        let a = ff64::try_from(f64::NAN);
+        assert!(a.is_err());
+        assert_eq!(a.unwrap_err(), ErrorTryFrom::CannotFixNan);
+    }
+
+    #[test]
+    fn try_from_inf() {
+        let a = ff64::try_from(f64::INFINITY);
+        assert!(a.is_err());
+        assert_eq!(a.unwrap_err(), ErrorTryFrom::CannotFixInfinite);
+
+        let b = ff64::try_from(f64::NEG_INFINITY);
+        assert!(b.is_err());
+        assert_eq!(b.unwrap_err(), ErrorTryFrom::CannotFixInfinite);
+    }
+}
